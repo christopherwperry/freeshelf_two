059481194e19c88c52362769b74881fc65042142
christopherwperry <christopherwperry@gmail.com>
2017-10-04 20:47:01 -0400

    Add index to books table for user that creates entry. Update relationship for user and books in corresponding models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_secure_password
+  has_many :books
+
+  validates_presence_of :username
+  validates_presence_of :password
+  validates_uniqueness_of :username
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171005003005) do
+ActiveRecord::Schema.define(version: 20171005003757) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20171005003005) do
     t.string "URL"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_books_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
